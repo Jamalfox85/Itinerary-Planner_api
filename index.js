87c48e7b0d8c3c7f64b0a9e28b2ec0e5028e5826
Jamal <Fox.Jamal@outlook.com>
2024-03-25 09:41:28 +0100

    port change back

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const itineraryRoutes = require("./routes/itinerary");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 3100;
+const PORT = process.env.PORT || 3000;
 
 // Cors
 const corsOptions = {
